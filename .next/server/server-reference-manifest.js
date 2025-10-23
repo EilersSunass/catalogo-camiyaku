c471b531b2c1b731fe612bb86fe2d56a7011f74a
EilersSunass <geilers@sunass.gob.pe>
2025-10-23 14:16:01 -0500

    Primeros archivos

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Wtm4CHcXBykLPfIuvNkP1KV44WT5Fvio0Sb3U8vKKr8=\"}"
---
+++ .next/static/chunks/app/(dashboard)/products/[id]/edit/page-25efc23941ef4942.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[389],{},function(n){n.O(0,[673,994,530,462,450,189,971,69,744],function(){return n(n.s=6189)}),_N_E=n.O()}]);
